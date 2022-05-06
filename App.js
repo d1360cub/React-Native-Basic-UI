e2b772f15b30f4bd00219147f6baf2010fdadf33
ojosdegatonegro <diegofernandocubides@gmail.com>
2022-05-05 22:02:57 -0500

    adds tags and styles

--- App.js
+++ App.js
@@ -23,6 +23,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 20,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
--- components/Card.jsx
+++ components/Card.jsx
@@ -4,13 +4,20 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 function Card(props) {
   const {author, picture, about, tags, comments}= props;
   return (
-    <View style={styles.container}>
+    <View style={styles.containerCard}>
       <View style={styles.imageContainer}>
-        <Image source={{uri: 'picture'}} />
+        <Image style={{
+            resizeMode: "contain",
+            height: 100,
+            width: 200
+          }} source={require('../assets/favicon.png')} />
       </View>
       <View style={styles.textContainer}>
         <Text style={styles.aboutText}>{about}</Text>
         <Text style={styles.titleText}> by {author}</Text>
+        <View style={styles.tagsContainer}>
+          {tags.map((element) => <Text style={styles.tagsText}>{element}</Text>)}
+        </View>
         <Text>{comments.length} Comments</Text>
       </View>
     </View>
@@ -18,32 +25,42 @@ function Card(props) {
 }
 
 const styles = StyleSheet.create({
-  container: {
+  containerCard: {
     flex: 1,
     flexDirection: 'row',
-    //backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 5,
   },
   imageContainer: {
     flex: 1,
-    backgroundColor: '#f1f',
     alignItems: 'center',
     justifyContent: 'center',
+    marginRight: 5,
   },
   textContainer: {
     flex: 1,
-    backgroundColor: '#ff1',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   aboutText: {
     textAlign: 'justify',
+    paddingTop: 15,
+    paddingRight: 5,
+    paddingLeft: 5,
   },
   titleText: {
     fontSize: 20,
-    fontWeight: "bold"
-  }
+    fontWeight: 'bold',
+  },
+  tagsContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    
+  },
+  tagsText: {
+    margin: 2,
+    borderRadius: 5,
+    backgroundColor: `#6495ed`,
+    padding: 10,
+    fontWeight: 'bold',
+  },
 });
 
 export default Card;
